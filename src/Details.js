85303fc0fb589666a6a8ee3d7910862a591c10a2
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-10-12 19:57:17 -0700

    Async UI
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,60 @@
 import React from 'react';
+import pf from 'petfinder-client';
+import { navigate } from '@reach/router';
+
+import Carousel from './Carousel';
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    petfinder.pet
+      .get({ output: 'full', id: this.props.id })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          media: pet.media,
+          description: pet.description,
+          breed: Array.isArray(pet.breeds.breed)
+            ? pet.breeds.breed.join(', ')
+            : pet.breeds.breed,
+          loading: false
+        });
+      })
+      .catch(() => {
+        navigate('/');
+      });
+  }
+
   render() {
-    return <h1>hi lol</h1>;
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { name, animal, breed, location, media, description } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
